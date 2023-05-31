4ddbc0c3333f76da589a95b9286e8f03fd384967
Poogal0 <golubchikov_2002@mail.ru>
2023-05-31 23:28:46 +0400

    v2 - added QLabel

--- main.cpp
+++ main.cpp
@@ -1,8 +1,23 @@
-#include <QApplication>
-#include <QWidget>
-int main(int argc, char *argv[]) {
-    QApplication app(argc, argv);
-    QWidget window;
-    window.show();
-    return app.exec();
+#include <QtWidgets>
+#include <QDebug>
+class MainWindow : public QMainWindow {
+public:
+MainWindow();
+private:
+QLabel * textLabel;
+};
+
+MainWindow::MainWindow(){
+setWindowTitle("Pract.15");
+textLabel = new QLabel(this);
+textLabel -> setText("Text QLabel");
+textLabel ->setGeometry(50,100,350,120);
+textLabel ->setFont(QFont("Time", 28, QFont::Bold));
+}
+
+int main (int argc , char *argv[]){
+	QApplication app (argc , argv);
+	MainWindow mainWin;
+	mainWin.show();
+	return app.exec();
 }
