1156839639052609e11211665f3693bd773e9583
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 09:08:06 +0400

    v4 - working config.ini

--- main.cpp
+++ main.cpp
@@ -1,56 +1,39 @@
 #include <QtWidgets>
 #include <QDebug>
-#define CONFIG_WIDTH "widthwindow"
-#define CONFIG_HEIGHT "heightwindow"
 
 class MainWindow : public QMainWindow{
     public:
         MainWindow();
     private:
-        QString filePath;
-        int heightWindow = 600;
-        int widthWindow = 400;
-        QString strWidth;
-        QString strHeight;
+        QString filePath ="../config.ini";
+        int heightWindow = 400;
+        int widthWindow = 100;
+        QString strWidth,strHeight;
         QString strConfig;
         QString strEqual="=";
-        QMap <QString, QString > mapConfig;
-};  
-    
+        QMap <QString,QString> mapConfig;
+    };
+
 MainWindow::MainWindow(){
-	filePath = "../config.ini";
-    QFile file (filePath);
-
-    
-
+    mapConfig.insert("widthwindow","500");
+    mapConfig.insert("heightwindow","500");
+    QFile file(filePath);
     if (!file.open(QFile::ReadOnly)){
-        QMessageBox::information (0, "Information", " Configuration File not found at path: \n " + filePath);
+        QMessageBox::information(0,"Information"," Configuration File \n not found at path: \n " +filePath);
     }
     while (!file.atEnd()){
         strConfig = file.readLine();
         int posEqual = strConfig.indexOf(strEqual);
-        if (!strConfig.left(posEqual).compare(CONFIG_WIDTH)){
-            strWidth = strConfig.right(strConfig.size() - posEqual - 1);
-        }
-        if (!strConfig.left(posEqual).compare(CONFIG_HEIGHT)){
-            strHeight = strConfig.right(strConfig.size() - posEqual - 1);
-        }
-        if (!mapConfig[strConfig.left(posEqual)].isEmpty()) {
-            mapConfig[strConfig.left(posEqual)] = strConfig.right(strConfig.size() - posEqual - 1);
+        if (!mapConfig[strConfig.left(posEqual)].isEmpty()){
+            mapConfig[strConfig.left(posEqual)]=strConfig.right(strConfig.size()-posEqual-1);
         }
     }
- 
-
     file.close();
-    widthWindow=strWidth.toInt();
-    heightWindow=strHeight.toInt();
-    widthWindow = mapConfig["widthwindow"].toInt();
-    heightWindow = mapConfig["heightwindow"].toInt();
-    qDebug()<<widthWindow;
-    qDebug()<<heightWindow;
+    widthWindow=mapConfig["widthwindow"].toInt();
+    heightWindow=mapConfig["heightwindow"].toInt();
     resize(widthWindow,heightWindow);
 }
-    
+
 int main(int argc,char *argv[]){
     QApplication app(argc,argv);
     MainWindow mainWin;
