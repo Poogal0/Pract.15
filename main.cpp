f0de699fcbb41b5043441c3d78c3d63cc05fd83c
Poogal0 <golubchikov_2002@mail.ru>
2023-06-01 09:21:54 +0400

    2 v1 - Bitmap proj.

--- main.cpp
+++ main.cpp
@@ -1,42 +1,95 @@
-#include <QtWidgets>
-#include <QDebug>
+#include <QApplication>
+#include <QWidget>
+#include <QLabel>
+#include <QVBoxLayout>
+#include <QFrame>
+#include <QFile>
+#include <QTextStream>
+#include <QPushButton>
+#include <QPainter>
+const int NUM_CELLS = 8;
 
-class MainWindow : public QMainWindow{
-    public:
-        MainWindow();
-    private:
-        QString filePath ="../config.ini";
-        int heightWindow = 400;
-        int widthWindow = 100;
-        QString strWidth,strHeight;
-        QString strConfig;
-        QString strEqual="=";
-        QMap <QString,QString> mapConfig;
-    };
+class BitCell : public QLabel {
+public:
+    BitCell(QWidget *parent = nullptr) : QLabel(parent) {}
 
-MainWindow::MainWindow(){
-    mapConfig.insert("widthwindow","500");
-    mapConfig.insert("heightwindow","500");
-    QFile file(filePath);
-    if (!file.open(QFile::ReadOnly)){
-        QMessageBox::information(0,"Information"," Configuration File \n not found at path: \n " +filePath);
+    void setValue(bool value) {
+        m_value = value;
+        update();
     }
-    while (!file.atEnd()){
-        strConfig = file.readLine();
-        int posEqual = strConfig.indexOf(strEqual);
-        if (!mapConfig[strConfig.left(posEqual)].isEmpty()){
-            mapConfig[strConfig.left(posEqual)]=strConfig.right(strConfig.size()-posEqual-1);
+
+    bool value() const { return m_value; }
+
+protected:
+    void paintEvent(QPaintEvent *event) override {
+        QLabel::paintEvent(event);
+        QPainter painter(this);
+        painter.setPen(Qt::black);
+        painter.setFont(QFont("Arial", 12));
+        painter.drawText(rect(), Qt::AlignCenter, m_value ? "1" : "0");
+    }
+
+private:
+    bool m_value = false;
+};
+
+class BitMapWidget : public QWidget {
+public:
+    BitMapWidget(QWidget *parent = nullptr) : QWidget(parent) {
+        setFixedSize(400, 80);
+        setWindowTitle("BitMap v0.1");
+
+        m_cells.resize(NUM_CELLS);
+        for (int i = 0; i < NUM_CELLS; i++) {
+            m_cells[i] = new BitCell(this);
+            m_cells[i]->setFixedSize(40, 40);
+        }
+
+        QPushButton *reloadButton = new QPushButton("Reload", this);
+        connect(reloadButton, &QPushButton::clicked, this, &BitMapWidget::rel);
+
+        QVBoxLayout *layout = new QVBoxLayout;
+        QHBoxLayout *cellsLayout = new QHBoxLayout;
+        cellsLayout->setSpacing(5);
+        cellsLayout->setMargin(0);
+        for (int i = 0; i < NUM_CELLS; i++) {
+            cellsLayout->addWidget(m_cells[i]);
+        }
+        layout->addLayout(cellsLayout);
+        layout->addWidget(reloadButton);
+        setLayout(layout);
+
+        rel();
+    }
+
+    void rel() {
+        QFile file("config.ini");
+        if (file.open(QIODevice::ReadOnly | QIODevice::Text)) {
+            QTextStream in(&file);
+            QString line = in.readLine();
+            if (!line.isEmpty()) {
+                bool ok = false;
+                quint8 byte = line.toUInt(&ok, 16);
+                if (ok) {
+                    for (int i = 0; i < NUM_CELLS; i++) {
+                        m_cells[i]->setValue(byte & (1 << (NUM_CELLS - i - 1)));
+                    }
+                }
+            }
+            file.close();
         }
     }
-    file.close();
-    widthWindow=mapConfig["widthwindow"].toInt();
-    heightWindow=mapConfig["heightwindow"].toInt();
-    resize(widthWindow,heightWindow);
-}
 
-int main(int argc,char *argv[]){
-    QApplication app(argc,argv);
-    MainWindow mainWin;
-    mainWin.show();
-    return app.exec();
+private:
+    QVector<BitCell*> m_cells;
+};
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+
+    BitMapWidget w;
+    w.show();
+
+    return a.exec();
 }
